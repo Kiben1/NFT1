af08dc5beb91ef9160f79e1bd8d6328d56f99807
Kiben1 <laimeperezkiben@gmail.com>
2024-08-04 02:07:20 -0400

    first commit3

--- packages/hardhat/contracts/MyNft.sol
+++ packages/hardhat/contracts/MyNft.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 // Compatible with OpenZeppelin Contracts ^5.0.0
 pragma solidity ^0.8.20;
-
+//hola esta difici
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
