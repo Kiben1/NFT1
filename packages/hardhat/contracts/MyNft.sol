38fa85e3c8d5859adb3778962434fa05ea9422cf
Kiben1 <laimeperezkiben@gmail.com>
2024-08-04 04:50:59 -0400

    first commit6

--- packages/hardhat/contracts/MyNft.sol
+++ packages/hardhat/contracts/MyNft.sol
@@ -9,7 +9,23 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract MyNFT is ERC721, ERC721URIStorage, Ownable {
 	uint256 private _nextTokenId;
 
-	constructor() ERC721("MyToken", "MTK") Ownable() {}
+	constructor(address initialOwner)
+        ERC721("CursoSolidity2024ETHB", "MTK")
+        Ownable()
+    {
+        transferOwnership(initialOwner);
+        mintInitialTokens();
+    }
+	function mintInitialTokens() internal onlyOwner {
+        _mintAndSetURI(0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC, "Qmd8LpMe2eayE4WJQanLGHrwz7wjq8qMEXyU3dex6TXzc4");
+        _mintAndSetURI(0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC, "Qmd8LpMe2eayE4WJQanLGHrwz7wjq8qMEXyU3dex6TXzc4");
+		_mintAndSetURI(0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC, "Qmd8LpMe2eayE4WJQanLGHrwz7wjq8qMEXyU3dex6TXzc4");
+		_mintAndSetURI(0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC, "Qmd8LpMe2eayE4WJQanLGHrwz7wjq8qMEXyU3dex6TXzc4");
+    }
+
+    function _mintAndSetURI(address to, string memory uri) internal {
+        safeMint(to, uri);
+    }
 
 	function safeMint(address to, string memory uri) public onlyOwner {
 		uint256 tokenId = _nextTokenId++;
@@ -18,22 +34,33 @@ contract MyNFT is ERC721, ERC721URIStorage, Ownable {
 	}
 
 	// The following functions are overrides required by Solidity.
+	function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
 
-	function tokenURI(
-		uint256 tokenId
-	) public view override(ERC721, ERC721URIStorage) returns (string memory) {
-		return super.tokenURI(tokenId);
-	}
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 
-	function supportsInterface(
-		bytes4 interfaceId
-	) public view override(ERC721) returns (bool) {
-		return super.supportsInterface(interfaceId);
-	}
+    function _burn(uint256 tokenId) internal override (ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
 
-	function _burn(
-		uint256 tokenId
-	) internal override(ERC721, ERC721URIStorage) {
-		super._burn(tokenId);
-	}
+    function buyNFT(uint256 tokenId) public payable {
+        require(msg.value >= 0.1 ether, "Insufficient payment");
+        address owner = ownerOf(tokenId);
+        require(owner != msg.sender, "Cannot buy your own NFT");
+        
+        _transfer(owner, msg.sender, tokenId);
+    }
 }
